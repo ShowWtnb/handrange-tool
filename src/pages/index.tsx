d7cfbf8350a15907affefd6e91e7c8a75f6f521f
ShowWtnb <sho9021watanabe@gmail.com>
2023-06-06 14:47:34 +0900

    Remove Embed Youtube

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import HandRangeToolHome from '@/component/HandRangeToolHome'
-import { Grid } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 
 export default function Home() {
   return (
@@ -15,11 +15,16 @@ export default function Home() {
         <Grid item xs={12}>
           <HandRangeToolHome />
         </Grid>
-        <Grid item xs={1}/>
-        <Grid item xs={10}>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/7vudIk1J_g0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <Grid item xs={2} >
+          <Typography >
+            {'参考動画：'}
+          </Typography>
         </Grid>
-        <Grid item xs={1}/>
+        <Grid item xs={8}>
+          {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/7vudIk1J_g0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+        <Button target="_blank" href="https://youtu.be/7vudIk1J_g0">【初公開】ヨコサワが実際に使っているハンドランキングがこちらです。</Button>
+        </Grid>
+        <Grid item xs={2}/>
       </Grid>
     </div>
   )
